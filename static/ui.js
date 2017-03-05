28ec8b668a9637cf6f0e7cefa1b46c631e561ffe
Jason Jalufka <jrj105@txstate.edu>
2017-03-05 00:51:56 -0600

    Highlight current configuration stage

--- static/ui.js
+++ static/ui.js
@@ -5,18 +5,25 @@ $( document ).ready(function() {
     console.log('ready!');
 
     $(document).ready(function(){
+        $('.btn-left').css("background-color", "#25bbf0");
         $('.stop-select').show();
         $('.distance-select').hide();
         $('.alert-select').hide();
     });
 
     $('.btn-left').click(function () {
+        $('.btn-left').css("background-color", "#25bbf0");
+        $('.btn-mid').css("background-color", "rgba(255,255,255,0.7)");
+        $('.btn-right').css("background-color", "rgba(255,255,255,0.7)");
         $('.stop-select').show();
         $('.distance-select').hide();
         $('.alert-select').hide();
     });
 
     $('.btn-mid, #stop-next').click(function () {
+        $('.btn-left').css("background-color", "rgba(255,255,255,0.7)");
+        $('.btn-mid').css("background-color", "#25bbf0");
+        $('.btn-right').css("background-color", "rgba(255,255,255,0.7)");
         $('.stop-select').hide();
         $('.distance-select').show();
         $('#distanceInput').focus();
@@ -24,6 +31,9 @@ $( document ).ready(function() {
     });
 
     $('.btn-right, #distance-next').click(function () {
+        $('.btn-left').css("background-color", "rgba(255,255,255,0.7)");
+        $('.btn-mid').css("background-color", "rgba(255,255,255,0.7)");
+        $('.btn-right').css("background-color", "#25bbf0");
         $('.stop-select').hide();
         $('.distance-select').hide();
         $('.alert-select').show();
