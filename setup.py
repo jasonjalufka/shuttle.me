0f03843b1fdd0915e6e12d85f987b608afc062b9
Jason Jalufka <jrj105@txstate.edu>
2017-03-14 01:24:00 -0500

    Remove excess whitespace

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-setup  (
-    name        = 'doublemap',
-    version     = '0.0.1',
-    description = 'An API client for DoubleMap.',
-    author = 'Travis Cunningham',
-    author_email = 'travcunn@umail.iu.edu',
-    url = 'https://github.com/travcunn/doublemap',
-    license = 'MIT',
-    packages  =  find_packages('src'),
-    package_dir = {'' : 'src'},
-    entry_points = {
+setup(
+    name='doublemap',
+    version='0.0.1',
+    description='An API client for DoubleMap.',
+    author='Travis Cunningham',
+    author_email='travcunn@umail.iu.edu',
+    url='https://github.com/travcunn/doublemap',
+    license='MIT',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    entry_points={
         'console_scripts': [
             'doublemap = doublemap.__init__:main',
         ],
