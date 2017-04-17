bde4fc0042963d2895b76be2e54ab35a37be0f78
abigailbarron <abbyeb22@gmail.com>
2017-04-17 17:49:34 -0500

    Add code to find several buses heading to user's stop

--- api.py
+++ api.py
@@ -9,18 +9,24 @@ from init import app
 closest_bus = {}
 closest_buses = []
 
+# Moved these here because it only needs to be retrieved once
+#print preferences["stop"]
+# print preferences.get("stop")
+# stopLat = tracker.stop_info(int(preferences.get("stop")))["lat"]
+# stopLon = tracker.stop_info(int(preferences.get("stop")))["lon"]
+# fullPath = tracker.route_info(preferences["route"])["path"]
+# pairs = zip(fullPath[::2], fullPath[1::2])
+
+
 @app.route('/_update_preferences', methods=['POST'])
 def update_preferences():
     data = request.get_json()
 
 @app.route('/_get_arrival_time', methods=['GET', 'POST'])
 def get_route():
-    print "hitta i made it to get arrival time"
 
     #get list of running buses from ajax
     buses = request.args.getlist('buses[]')
-    print "hitta i was able to get your data of buses"
-    print buses
     get_closest_bus(buses)
     if not closest_buses:
         print "is this why"
@@ -29,69 +35,129 @@ def get_route():
         # we have a bus coming!
         print "we have a bus coming!"
 
-        locations = []
+        #locations = []
 
         stop = preferences.get("stop")
         route = preferences.get("route")
 
+        response = {}
         # get bus lat and lon
         print "closest buses!"
         #print closest_bus["id"]
-        print closest_buses
+        #print closest_buses
 
-        startLat = tracker.bus_info(int(closest_bus["id"]))["lat"]
+        response["number"]=len(closest_buses)
+
+        for bus in closest_buses:
+            bus["formattedTime"] = get_eta(bus)
+
+        response["buses"] = closest_buses
+        print response
+        #startLat = tracker.bus_info(int(closest_bus["id"]))["lat"]
         #print startLat
 
-        startLon = tracker.bus_info(int(closest_bus["id"]))["lon"]
+        #startLon = tracker.bus_info(int(closest_bus["id"]))["lon"]
         #print startLon
 
         # last stop lat lon
-        stopLat = tracker.stop_info(int(stop))["lat"]
-        stopLon = tracker.stop_info(int(stop))["lon"]
 
-        fullPath = tracker.route_info(route)["path"]
 
         # separate fullPath into coordinate lat, lon pairs
-        pairs = zip(fullPath[::2], fullPath[1::2])
-        print "Pairs"
-        print len(pairs)
+        #print "Pairs"
+        #print len(pairs)
 
         # find coordinate closest to stop coordinate in route
-        dist = lambda s, d: (s[0] - d[0]) ** 2 + (s[1] - d[1]) ** 2
-        startCoord = (startLat, startLon)
-        stopCoord = (stopLat, stopLon)
-        firstPair = min(pairs, key=partial(dist, startCoord))
-        lastPair = min(pairs, key=partial(dist, stopCoord))
+        # dist = lambda s, d: (s[0] - d[0]) ** 2 + (s[1] - d[1]) ** 2
+        # startCoord = (startLat, startLon)
+        # stopCoord = (stopLat, stopLon)
+        # firstPair = min(pairs, key=partial(dist, startCoord))
+        # lastPair = min(pairs, key=partial(dist, stopCoord))
 
         # index of closest coordinate to stop
         # tells us last coordinate to load
-        startLocationIndex = pairs.index(firstPair)
-        print startLocationIndex
-        endLocationIndex = pairs.index(lastPair)
-        print endLocationIndex
-        print "TakeSpread:"
-        locationGenerator = takespread(range(startLocationIndex,endLocationIndex), 23)
+        #startLocationIndex = pairs.index(firstPair)
+        #print startLocationIndex
+        #endLocationIndex = pairs.index(lastPair)
+        #print endLocationIndex
+        #print "TakeSpread:"
+        #locationGenerator = takespread(range(startLocationIndex,endLocationIndex), 23)
         # locationGenerator = takespread(range(1, len(pairs)-1), 23)
+        # for i in locationGenerator:
+        #     test = str(pairs[i][0]) + ", " + str(pairs[i][1])
+        #     locations.append(test)
+
+        # insert starting location location as first location element
+        # locations.append(str(stopLat) + ", " + str(stopLon))
+        # insert user
+        #locations.insert(0, str(startLat) + ", " + str(startLon))
+        # locations.insert(str(pairs[endLocationIndex][0]) + ", " + str(pairs[endLocationIndex][1]))
+
+        #print locations[0]
+        #print locations
+
+        #url = 'https://www.mapquestapi.com/directions/v2/route?json={"locations":["%s"]}&timeType=1&useTraffic=true\
+        #&outFormat=json&key=tAY5u0ki3CMdkv7GoGxT7ctvXEaKCSX9' % '", "'.join(map(str, locations))
+
+        #response = requests.get(url).json()
+            # return flask.redirect(url_for('/'))
+        return jsonify(response)
+
+
+# function that gets the ETA for each bus heading your way
+
+def get_eta(bus):
+    print bus
+    stopLat = tracker.stop_info(int(preferences.get("stop")))["lat"]
+    stopLon = tracker.stop_info(int(preferences.get("stop")))["lon"]
+    fullPath = tracker.route_info(preferences["route"])["path"]
+    pairs = zip(fullPath[::2], fullPath[1::2])
+    pairs = pairs[0:-10]
+    locations = []
+
+    startLat = tracker.bus_info(int(bus["id"]))["lat"]
+    startLon = tracker.bus_info(int(bus["id"]))["lon"]
+
+    dist = lambda s, d: (s[0] - d[0]) ** 2 + (s[1] - d[1]) ** 2
+    startCoord = (startLat, startLon)
+    stopCoord = (stopLat, stopLon)
+
+    print "startCord"
+    print startCoord
+    firstPair = min(pairs, key=partial(dist, startCoord))
+    print "first pair"
+    print firstPair
+    lastPair = min(pairs, key=partial(dist, stopCoord))
+
+    startLocationIndex = pairs.index(firstPair)
+    endLocationIndex = pairs.index(lastPair)
+
+    # check to see the range before trying to split it up
+    if (endLocationIndex - startLocationIndex) < 25:
+        print "im pretty close"
+        # do this
+        for i in range(startLocationIndex, endLocationIndex):
+            test = str(pairs[i][0]) + ", " + str(pairs[i][1])
+            locations.append(test)
+    else:
+        print "i'm pretty far"
+        locationGenerator = takespread(range(startLocationIndex, endLocationIndex), 23)
         for i in locationGenerator:
             test = str(pairs[i][0]) + ", " + str(pairs[i][1])
             locations.append(test)
-
-        # insert starting location location as first location element
         locations.append(str(stopLat) + ", " + str(stopLon))
-        # insert user
         locations.insert(0, str(startLat) + ", " + str(startLon))
-        # locations.insert(str(pairs[endLocationIndex][0]) + ", " + str(pairs[endLocationIndex][1]))
+    print "location length"
+    print len(locations)
+    url = 'https://www.mapquestapi.com/directions/v2/route?json={"locations":["%s"]}&timeType=1&useTraffic=true\
+            &outFormat=json&key=tAY5u0ki3CMdkv7GoGxT7ctvXEaKCSX9' % '", "'.join(map(str, locations))
 
-        print locations[0]
-        print locations
-
-        url = 'https://www.mapquestapi.com/directions/v2/route?json={"locations":["%s"]}&timeType=1&useTraffic=true\
-        &outFormat=json&key=tAY5u0ki3CMdkv7GoGxT7ctvXEaKCSX9' % '", "'.join(map(str, locations))
-
-        response = requests.get(url).json()
-        print response
-            # return flask.redirect(url_for('/'))
-        return jsonify(response)
+    print "URL"
+    print url
+    urlresponse = requests.get(url).json()
+    print "url response"
+    print urlresponse
+    print urlresponse["route"]["formattedTime"]
+    return urlresponse["route"]["formattedTime"]
 
 
 # choose num elements from sequence distributed as evenly as possible
@@ -129,7 +195,7 @@ def get_closest_bus(buses):
             #print bus_obj
             bus_info.update(bus_obj)
 
-            closest_buses.append(bus_obj)
+            closest_buses.append(bus_obj[bus])
 
     # there is a bus somewhere at UAC through user stop
     if bus_coming:
