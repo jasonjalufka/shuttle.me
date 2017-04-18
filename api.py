cefad4a8de4766d9f65ca7f8272a7eecef391b10
abigailbarron <abbyeb22@gmail.com>
2017-04-17 19:03:01 -0500

    Add code to find several buses heading to user's stop

--- api.py
+++ api.py
@@ -2,20 +2,13 @@ from views import tracker, preferences
 from flask import jsonify, request, redirect, url_for, abort
 import flask
 from math import ceil
-import requests
+import requests, json
 from functools import partial
 from init import app
 
 closest_bus = {}
 closest_buses = []
 
-# Moved these here because it only needs to be retrieved once
-#print preferences["stop"]
-# print preferences.get("stop")
-# stopLat = tracker.stop_info(int(preferences.get("stop")))["lat"]
-# stopLon = tracker.stop_info(int(preferences.get("stop")))["lon"]
-# fullPath = tracker.route_info(preferences["route"])["path"]
-# pairs = zip(fullPath[::2], fullPath[1::2])
 
 
 @app.route('/_update_preferences', methods=['POST'])
@@ -24,27 +17,28 @@ def update_preferences():
 
 @app.route('/_get_arrival_time', methods=['GET', 'POST'])
 def get_route():
+    print "called in get arrival time"
+    del closest_buses[:]
 
     #get list of running buses from ajax
     buses = request.args.getlist('buses[]')
-    get_closest_bus(buses)
-    if not closest_buses:
+    print "was i able to get the buses?"
+    #bus_on_way = get_closest_bus(buses)
+    bus_on_way = False
+    print "did this getclosestbus function upset it?"
+    if not bus_on_way:
         print "is this why"
-        abort(401)
+        return "error"
     else:
         # we have a bus coming!
         print "we have a bus coming!"
-
-        #locations = []
-
         stop = preferences.get("stop")
         route = preferences.get("route")
 
         response = {}
         # get bus lat and lon
         print "closest buses!"
-        #print closest_bus["id"]
-        #print closest_buses
+        print closest_buses
 
         response["number"]=len(closest_buses)
 
@@ -53,54 +47,8 @@ def get_route():
 
         response["buses"] = closest_buses
         print response
-        #startLat = tracker.bus_info(int(closest_bus["id"]))["lat"]
-        #print startLat
 
-        #startLon = tracker.bus_info(int(closest_bus["id"]))["lon"]
-        #print startLon
-
-        # last stop lat lon
-
-
-        # separate fullPath into coordinate lat, lon pairs
-        #print "Pairs"
-        #print len(pairs)
-
-        # find coordinate closest to stop coordinate in route
-        # dist = lambda s, d: (s[0] - d[0]) ** 2 + (s[1] - d[1]) ** 2
-        # startCoord = (startLat, startLon)
-        # stopCoord = (stopLat, stopLon)
-        # firstPair = min(pairs, key=partial(dist, startCoord))
-        # lastPair = min(pairs, key=partial(dist, stopCoord))
-
-        # index of closest coordinate to stop
-        # tells us last coordinate to load
-        #startLocationIndex = pairs.index(firstPair)
-        #print startLocationIndex
-        #endLocationIndex = pairs.index(lastPair)
-        #print endLocationIndex
-        #print "TakeSpread:"
-        #locationGenerator = takespread(range(startLocationIndex,endLocationIndex), 23)
-        # locationGenerator = takespread(range(1, len(pairs)-1), 23)
-        # for i in locationGenerator:
-        #     test = str(pairs[i][0]) + ", " + str(pairs[i][1])
-        #     locations.append(test)
-
-        # insert starting location location as first location element
-        # locations.append(str(stopLat) + ", " + str(stopLon))
-        # insert user
-        #locations.insert(0, str(startLat) + ", " + str(startLon))
-        # locations.insert(str(pairs[endLocationIndex][0]) + ", " + str(pairs[endLocationIndex][1]))
-
-        #print locations[0]
-        #print locations
-
-        #url = 'https://www.mapquestapi.com/directions/v2/route?json={"locations":["%s"]}&timeType=1&useTraffic=true\
-        #&outFormat=json&key=tAY5u0ki3CMdkv7GoGxT7ctvXEaKCSX9' % '", "'.join(map(str, locations))
-
-        #response = requests.get(url).json()
-            # return flask.redirect(url_for('/'))
-        return jsonify(response)
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
 
 # function that gets the ETA for each bus heading your way
@@ -148,6 +96,7 @@ def get_eta(bus):
         locations.insert(0, str(startLat) + ", " + str(startLon))
     print "location length"
     print len(locations)
+    print locations
     url = 'https://www.mapquestapi.com/directions/v2/route?json={"locations":["%s"]}&timeType=1&useTraffic=true\
             &outFormat=json&key=tAY5u0ki3CMdkv7GoGxT7ctvXEaKCSX9' % '", "'.join(map(str, locations))
 
@@ -197,19 +146,7 @@ def get_closest_bus(buses):
 
             closest_buses.append(bus_obj[bus])
 
-    # there is a bus somewhere at UAC through user stop
-    if bus_coming:
-        # there was a previous closest_bus
-        if not closest_bus:
-            # assign the first bus as the closest bus
-            closest_bus.update(bus_info[bus_info.keys()[0]])
-            for key, bus in bus_info.iteritems():
-                if closest_bus["index"] < bus["index"]:
-                    closest_bus.clear()
-                    closest_bus.update(bus)
-                    #print closest_bus
-    #print closest_bus
-    return
+    return bus_coming
 
 
 
